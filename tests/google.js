2b7d35f8bb253cb1f8d2dc723cdac662996b17b1
roman_gonzalez <roman.gonzalez@amplemind.com>
2022-04-14 16:30:24 -0500

    added url validation tests

--- tests/google.js
+++ tests/google.js
@@ -16,17 +16,17 @@ module.exports = {
     browser
         .useXpath()
         .url('https://www.google.com/advanced_search')
-        .waitForElementVisible(mainQueryInputSelector)
         .setValue(mainQueryInputSelector, mainQuery)
-        .waitForElementVisible(languageSelector)
         .click(languageSelector)
         .click(languageValueSelector)
-        .waitForElementVisible(lastUpdatedSelector)
         .click(lastUpdatedSelector)
         .click(lastUpdatedValueSelector)
         .click(submitBttn)
-        .assert.elementPresent(mainQueryResult)
-        .assert.elementPresent(languageResult)
-        .assert.elementPresent(lastUpdatedResult)
+        .assert.elementPresent(mainQueryResult, 'Page displays Zelda as query')
+        .assert.elementPresent(languageResult, 'Page displays Italian as language')
+        .assert.elementPresent(lastUpdatedResult, 'Page displays last month as last updated')
+        .assert.urlContains('as_q=Zelda', 'Query is Zelda')
+        .assert.urlContains('lr=lang_it', 'Language is Italian')
+        .assert.urlContains('as_qdr=m', 'Time period is last month')
     }
 }
